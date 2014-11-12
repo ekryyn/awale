64c696f9171c41a9d0423c91ec18f9575f97f9b9
freddy <Freddy.Teuma@smartodds.co.uk>
2014-11-12 16:53:01 +0000

    Add simple standalone server runner

---
+++ standalone_server.py
@@ -0,0 +1,23 @@
+import sys
+from server import AwlServer
+import client
+from gui.console import Console
+import time
+
+def create_server():
+    s = AwlServer()
+    s.daemon = True
+    return s
+
+if __name__ == '__main__':
+    gui = Console()
+    s = create_server()
+    try:
+        s.start()
+        while True:
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Shutting down...")
+        s.running = False
+        s.join()
+
