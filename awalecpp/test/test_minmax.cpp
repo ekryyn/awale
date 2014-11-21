dfe93c07609c0dd23af1f83175b255a414fbfd64
benlinuxPublic <benoit_jottreau@yahoo.fr>
2014-11-21 00:04:55 +0000

    fixed minmax added streaming

--- awalecpp/test/test_minmax.cpp
+++ awalecpp/test/test_minmax.cpp
@@ -20,8 +20,9 @@ BOOST_AUTO_TEST_CASE(test_constructor)
 {
 
 
-    std::stringstream s("1 2 0 1\n5 1\n0\n0 1\n");
+    //std::stringstream s("1 2 0 1\n5 1\n0\n0 1\n");
 
+	std::stringstream s("0 2 0 1\n5 1\n0\n1\n");
 
     game game_state(s);
 
@@ -41,4 +42,69 @@ BOOST_AUTO_TEST_CASE(test_constructor)
     BOOST_CHECK_EQUAL(move_values.size(), game_state.valid_moves.size());
 
 
+	
+
 }
+
+
+BOOST_AUTO_TEST_CASE(test_constructor1)
+{
+
+
+    //std::stringstream s("1 2 0 1\n5 1\n0\n0 1\n");
+
+	std::stringstream s("1 1 0 1\n5 1\n0\n0 1\n");
+
+    game game_state(s);
+
+
+
+    int depth = 10;
+    basic_minmax<game, decltype(heuristic)> strat(game_state, heuristic, depth);
+
+    auto move_values = strat.compute_move_values();
+
+    std::cout <<game_state << std::endl;
+
+    for (int i = 0; i< move_values.size(); i++) {
+        std::cout <<game_state.valid_moves[i] << "  " << move_values[i] <<std::endl;
+    }
+
+    BOOST_CHECK_EQUAL(move_values.size(), game_state.valid_moves.size());
+
+
+	
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test_constructor2)
+{
+
+
+    //std::stringstream s("1 2 0 1\n5 1\n0\n0 1\n");
+
+	std::stringstream s("4 4 4 4 4 4 4 4 4 4 4 4\n0 0\n0\n0 1 2 3 4 5\n");
+
+    game game_state(s);
+
+
+
+    int depth = 9;
+    basic_minmax<game, decltype(heuristic)> strat(game_state, heuristic, depth);
+
+    auto move_values = strat.compute_move_values();
+
+    std::cout <<game_state << std::endl;
+
+    for (int i = 0; i< move_values.size(); i++) {
+        std::cout <<game_state.valid_moves[i] << "  " << move_values[i] <<std::endl;
+    }
+
+    BOOST_CHECK_EQUAL(move_values.size(), game_state.valid_moves.size());
+
+
+	
+
+}
+
